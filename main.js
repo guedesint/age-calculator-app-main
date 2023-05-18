e8eb690f4550e4528967d29b2b32518ca728d43e
GuedesNotbook <guedesint@gmail.com>
2023-05-18 14:44:54 -0300

    começando testa array

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const botao = document.querySelector('.botaoTeste');
 
 
-
 function calculaIdade() {
+
   const diaNascimento = parseInt(document.querySelector('#dd').value);
   const mesNascimento = parseInt(document.querySelector('#mm').value);
   const anoNascimento = parseInt(document.querySelector('#yy').value);
@@ -11,9 +11,14 @@ function calculaIdade() {
 
   const anoAtual = dataAtual.getFullYear();
   const mesAtual = dataAtual.getMonth() + 1;
+  const diaAtual = dataAtual.getDate();
 
   let idade = anoAtual - anoNascimento;
-  
+  let idadeMes = (mesAtual - mesNascimento) + (idade * 12);
+  let idadeDia = (diaAtual - diaNascimento) + (idadeMes * 30);
+
+
+
   if (mesAtual < mesNascimento) {
     idade--;
   } else {
@@ -25,9 +30,22 @@ function calculaIdade() {
       }
     }
   }
-  
-  let meses = idade * 12;
-  let dias = meses * 31; 
+
+  if (diaNascimento && mesNascimento && anoNascimento >= 0) {
+
+    const resultadoAno = document.querySelector('.anos')
+    resultadoAno.innerHTML = idade;
+
+    const resultadoMes = document.querySelector('.meses')
+    resultadoMes.innerHTML = idadeMes;
+
+    const resultadoDia = document.querySelector('.dias')
+    resultadoDia.innerHTML = idadeDia;
+  }else {
+    
+  }
+
+
 }
 calculaIdade()
 

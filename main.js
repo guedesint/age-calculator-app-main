8ff6205cbe61eb390ae262130752d04047f27c99
GuedesNotbook <guedesint@gmail.com>
2023-05-30 09:28:44 -0300

    tem que arrumar os erros e validacao dos inputs

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-const botao = document.querySelector('.logo');
-const resultadoDia = document.querySelector('.dias')
-const resultadoMes = document.querySelector('.meses')
-const resultadoAno = document.querySelector('.anos')
+const botao = document.querySelector(".logo");
+const resultadoDia = document.querySelector(".dias");
+const resultadoMes = document.querySelector(".meses");
+const resultadoAno = document.querySelector(".anos");
 
-
-botao.addEventListener('click', () => {
+botao.addEventListener("click", () => {
   //Pegando valor dos Inputs
-  const diaNascimento = parseInt(document.querySelector('#dd').value);
-  const mesNascimento = parseInt(document.querySelector('#mm').value);
-  const anoNascimento = parseInt(document.querySelector('#yy').value);
-  
+  const diaNascimento = parseInt(document.querySelector("#dd").value);
+  const mesNascimento = parseInt(document.querySelector("#mm").value);
+  const anoNascimento = parseInt(document.querySelector("#yy").value);
+
   //Pegando datas Atuais
   const dataAtual = new Date();
   const diaAtual = dataAtual.getDate();
@@ -18,23 +17,24 @@ botao.addEventListener('click', () => {
 
   //Validando Valor Inputs
   const validaDia = diaNascimento > 0 && diaNascimento <= 31;
-  const validaMes = mesNascimento > 0 && mesNascimento <= 12;
+  const validaMes = mesNascimento > 0 && mesNascimento <= 12; 
   const validaAno = anoNascimento > 1900 && anoNascimento < dataAtual.getFullYear();
-
 
   if (validaDia && validaMes && validaAno) {
     let idade = anoAtual - anoNascimento;
     let idadeMes = (mesAtual + 12 - mesNascimento) % 12;
-    let idadeDia = diaAtual - diaNascimento
+    let idadeDia = diaAtual - diaNascimento;
 
     if (mesAtual <= mesNascimento || diaAtual < diaNascimento) {
       idade--;
     }
 
+    if (diaNascimento === diaAtual && mesNascimento === mesAtual) {
+      idade++;
+    }
+
     resultadoDia.innerHTML = idadeDia;
     resultadoMes.innerHTML = idadeMes;
     resultadoAno.innerHTML = idade;
-
   }
-
-})
+});

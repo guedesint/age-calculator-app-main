b48da11314f32ed8275266e10de1e35b69511e60
Gabriel PC <guedesint@gmail.com>
2023-07-02 17:15:50 -0300

    logica da calculadora concluida

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
 const botao = document.querySelector(".logo");
 const listaInputs = document.querySelectorAll(".lista-item");
 const resultadoDia = document.querySelector(".dias");
@@ -6,103 +8,91 @@ const resultadoAno = document.querySelector(".anos");
 const invalid = document.querySelector(".text-error");
 const erroTitulo = document.querySelector(".titulo-lista");
 const erroInput = document.querySelector(".input");
+let idade, idadeDia, idadeMes;
 
 botao.addEventListener("click", (event) => {
-      event.preventDefault();
+  event.preventDefault();
 
-      //Pegando valor dos Inputs
-      const diaNascimento = parseInt(document.querySelector("#dd").value);
-      const mesNascimento = parseInt(document.querySelector("#mm").value);
-      const anoNascimento = parseInt(document.querySelector("#yy").value);
+  //Pegando valor dos Inputs
+  const diaNascimento = parseInt(document.querySelector("#dd").value);
+  const mesNascimento = parseInt(document.querySelector("#mm").value);
+  const anoNascimento = parseInt(document.querySelector("#yy").value);
 
-      //Pegando datas Atuais
-      const dataAtual = new Date();
-      const diaAtual = dataAtual.getDate();
-      const mesAtual = dataAtual.getMonth() + 1;
-      const anoAtual = dataAtual.getFullYear();
+  //Pegando datas Atuais
+  const dataAtual = new Date();
+  const diaAtual = dataAtual.getDate();
+  const mesAtual = dataAtual.getMonth() + 1;
+  const anoAtual = dataAtual.getFullYear();
 
-      //Validando Valor Inputs
-      const validaDia = diaNascimento > 0 && diaNascimento <= 31;
-      const validaMes = mesNascimento > 0 && mesNascimento <= 12;
-      const validaAno = anoNascimento > 0 && anoNascimento <= anoAtual;
+  //Validando Valor Inputs
+  const validaDia = diaNascimento > 0 && diaNascimento <= 31;
+  const validaMes = mesNascimento > 0 && mesNascimento <= 12;
+  const validaAno = anoNascimento > 0 && anoNascimento <= anoAtual;
 
-      if (validaDia && validaMes && validaAno) {
-        const mesDiff = mesAtual - mesNascimento;
-        let idade = anoAtual - anoNascimento;
-        let idadeDia = diaAtual - diaNascimento;
-        let idadeMes = (mesAtual + 12 - mesNascimento) % 12;
+  if (validaDia && validaMes && validaAno) {
+    
+    if (
+      anoNascimento > anoAtual ||
+      (mesNascimento > mesAtual && anoNascimento == anoAtual) ||
+      (diaNascimento > diaAtual &&
+        mesNascimento == mesAtual &&
+        anoNascimento == anoAtual)
+    ) {
+      return;
+    }
 
-        if (mesDiff < 0) {
-          idade--;
-          mesDiff += 12;
-        }
+    idade = anoAtual - anoNascimento;
 
-        if (diaAtual < diaNascimento) {
-          if (idadeMes === 0) {
-            idade--;
-            idadeMes += 12;
-          }
-        }
+    if (mesAtual >= mesNascimento) {
+      idadeMes = mesAtual - mesNascimento;
+    } else {
+      idade--;
+      idadeMes = 12 + mesAtual - mesNascimento;
+    }
 
-        if (diaNascimento === diaAtual && mesNascimento === mesAtual) {
-          idadeDia = 0;
-        }
-        // if (idadeMes === idadeMes--) {
-        //   idadeMes = 0;
-        // }
-        switch (mesAtual) {
-          case 1:
-          case 2:
-          case 4:
-          case 6:
-          case 8:
-          case 9:
-          case 11:
-            // previous month has 31 days
-            idadeDia += 31;
-            break;
-          case 3:
-            // previous month has 28 days
-            idadeDia += 28;
-            break;
-          case 5:
-          case 7:
-          case 10:
-          case 12:
-            // previous month has 30 days
-            idadeDia += 30;
-            break;
-        }
-          
+    if (diaAtual >= diaNascimento) {
+      idadeDia = diaAtual - diaNascimento;
+    } else {
+      idadeMes--;
+      let days = months[mesAtual - 2];
+      idadeDia = days + diaAtual - diaNascimento;
+      if (idadeMes < 0) {
+        idadeMes = 11;
+        idade--;
+      }
+    }
+  }
 
-        resultadoDia.innerHTML = idadeDia;
-        resultadoMes.innerHTML = idadeMes;
-        resultadoAno.innerHTML = idade;
 
-        // function dataInvalida() {
+  resultadoDia.innerHTML = idadeDia;
+  resultadoMes.innerHTML = idadeMes;
+  resultadoAno.innerHTML = idade;
 
-        //   erroTitulo.classList.add("titulo-error");
-        //   erroInput.classList.add("input-error");
-        //   invalid.style.display = "block";
-        // }
+  function dataInvalida() {
 
-        // if (!validaDia || !validaMes || !validaAno) {
-        //   const tituloError = document.querySelectorAll('.titulo-lista');
-        //   const textError = document.querySelectorAll('.text-error');
-        //   const inputError = document.querySelectorAll('.input');
-        //   const erro = inputError + textError + tituloError;
+    erroTitulo.classList.add("titulo-error");
+    erroInput.classList.add("input-error");
+    invalid.style.display = "block";
+  }
 
-        //   tituloError.forEach((e) => {
-        //      e.classList.add('titulo-error');
-        //   });
+  if (!validaDia || !validaMes || !validaAno) {
+    const tituloError = document.querySelectorAll('.titulo-lista');
+    const textError = document.querySelectorAll('.text-error');
+    const inputError = document.querySelectorAll('.input');
+    const erro = inputError + textError + tituloError;
 
-        //   textError.forEach((e) => {
-        //      e.style.display = 'block'
-        //   });
+    tituloError.forEach((e) => {
+       e.classList.add('titulo-error');
+    });
 
-        //   inputError.forEach((e) => {
-        //      e.style.border = '1px solid hsl(0, 100%, 67%)'
-        //   });
+    textError.forEach((e) => {
+       e.style.display = 'block'
+    });
 
-        // }
-      }});
+    inputError.forEach((e) => {
+       e.style.border = '1px solid hsl(0, 100%, 67%)'
+    });
+
+    
+  }
+});
